043526454fce67b84f6b787e1191ecadfdcd31c1
Steven Lambert <2433219+straker@users.noreply.github.com>
2022-08-24 21:45:22 -0600

    feat: add steering behavior table (#10)
    * add steering behavior table
    
    * cleanup
    
    * typo
    
    * words

--- src/globals.js
+++ src/globals.js
@@ -2,7 +2,7 @@
   assign high use sub-properties to window to avoid using the main property name over and over again
 */
 export default function globals() {
-  let { PI, sin, cos, atan2, max, min } = Math
-  Object.assign(window, { PI, sin, cos, atan2, max, min })
+  let { PI, sin, cos, atan2 } = Math
+  Object.assign(window, { PI, sin, cos, atan2 })
 }
 globals()
---
+++ src/tables/behaviors.js
@@ -0,0 +1,49 @@
+import { angleToTarget, Vector } from '../libs/kontra.mjs'
+
+/*
+  steering behaviors that allow enemies to move in various ways.
+  all behaviors take same parameters: current enemy, player, and all enemies array.
+*/
+
+// avoidance (based on separation flocking behavior)
+// @see https://gamedevelopment.tutsplus.com/tutorials/3-simple-rules-of-flocking-behaviors-alignment-cohesion-and-separation--gamedev-3444
+function avoidance(source, targets, distance) {
+  let numNeighbors = 0,
+    avoidanceVector = Vector()
+
+  ;[].concat(targets).map(target => {
+    if (source != target && source.position.distance(target) <= distance) {
+      numNeighbors++
+      avoidanceVector = avoidanceVector.add(
+        target.position.subtract(source.position)
+      )
+    }
+  })
+
+  if (numNeighbors) {
+    avoidanceVector = avoidanceVector
+      .normalize(numNeighbors)
+      .scale(-1)
+      .normalize()
+  }
+
+  return avoidanceVector
+}
+
+const behaviors = [
+  // seek player. apply a weighted scale to the vector
+  // to make seeking player the lowest priority
+  // @see https://gamedevelopment.tutsplus.com/tutorials/understanding-steering-behaviors-seek--gamedev-849
+  (enemy, player) => {
+    let angle = angleToTarget(enemy, player)
+    return Vector(sin(angle), -cos(angle)).subtract(enemy.velocity).scale(0.9)
+  },
+
+  // avoid other enemies
+  distance => (enemy, player, enemies) => avoidance(enemy, enemies, distance),
+
+  // avoid player
+  distance => (enemy, player) => avoidance(enemy, player, distance)
+]
+
+export default behaviors
